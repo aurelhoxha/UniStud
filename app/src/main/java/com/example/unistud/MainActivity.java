09d802db67a1232b1c1173d50a5b663abfbdfa49
Aurel Hoxha <aureli.hoxha@gmail.com>
2019-03-13 19:48:18 +0300

    I fshiva

--- app/src/main/java/com/example/unistud/MainActivity.java
+++ app/src/main/java/com/example/unistud/MainActivity.java
@@ -4,11 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //Albjoni esht boss
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Aureli eshte Bos
     }
 }
