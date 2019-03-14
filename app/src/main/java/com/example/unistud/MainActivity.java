192f06ceba571cfe29d93cfda2cc434d66f22014
Aurel Hoxha <aureli.hoxha@gmail.com>
2019-03-14 03:38:34 +0300

    Working
    Login, SignUp and Forget Password Changed

--- app/src/main/java/com/example/unistud/MainActivity.java
+++ app/src/main/java/com/example/unistud/MainActivity.java
@@ -1,12 +1,112 @@
 package com.example.unistud;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Variables
+    private EditText mEmail, mPassword;
+    private Button mLoginButton;
+    private TextView mForgotPassword, mSignUp;
+    private ProgressDialog mDialog;
+
+    //Authentication to Firebase Instances
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+    private FirebaseUser mFirebaseUser;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Set the Variables
+        mEmail              = (EditText) findViewById(R.id.login_email_address);
+        mPassword           = (EditText) findViewById(R.id.login_password);
+        mLoginButton        = (Button)   findViewById(R.id.login_button);
+        mForgotPassword     = (TextView)   findViewById(R.id.forget_password);
+        mSignUp             = (TextView) findViewById(R.id.login_signup);
+        mDialog             = new ProgressDialog(this);
+
+        //Set the Listeners
+        mLoginButton.setOnClickListener(this);
+        mForgotPassword.setOnClickListener(this);
+        mSignUp.setOnClickListener(this);
+
+        mAuth         = FirebaseAuth.getInstance();
+
+        if( mAuth.getCurrentUser() != null ) {
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mLoginButton) {
+            userEmailSignIn();
+        }
+        else if(v == mForgotPassword) {
+            startActivity(new Intent(MainActivity.this,ForgotPassword.class));
+        }
+        else if(v == mSignUp) {
+            startActivity(new Intent(MainActivity.this,SignUpActivity.class));
+        }
+    }
+
+    private void userEmailSignIn(){
+        String userEmail = mEmail.getText().toString().trim();
+        String userPassword = mPassword.getText().toString().trim();
+
+        //Check For Empty
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(MainActivity.this,"Enter Email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(MainActivity.this,"Enter Password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mDialog.setMessage("Login please wait...");
+        mDialog.setIndeterminate(true);
+        mDialog.show();
+
+        mAuth.signInWithEmailAndPassword(userEmail,userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if( !task.isSuccessful() ){
+                    mDialog.dismiss();
+                    Toast.makeText(MainActivity.this,"Login not successful!",Toast.LENGTH_LONG).show();
+                }else{
+                    mDialog.dismiss();
+
+                    Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/unistud/SignUpActivity.java
+++ app/src/main/java/com/example/unistud/SignUpActivity.java
@@ -1,12 +1,101 @@
 package com.example.unistud;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //Variables
+    private EditText mEmail, mPassword, mConfirmPassword;
+    private Button mSignUpButton;
+    private TextView mSignInButton;
+
+    //Firebase Connection Instances
+    private FirebaseAuth mAuth;
+
+    //Other Variables
+    private ProgressDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        //Intialiaze the Variables
+        mEmail           = (EditText)findViewById(R.id.signup_email_address);
+        mPassword        = (EditText)findViewById(R.id.signup_password1);
+        mConfirmPassword = (EditText)findViewById(R.id.signup_password2);
+        mSignUpButton    = (Button)findViewById(R.id.signup_button);
+        mSignInButton    = (TextView)findViewById(R.id.signup_sign_in_button);
+        mAuth = FirebaseAuth.getInstance();
+        mDialog = new ProgressDialog(this);
+
+        //Set the Listeners
+        mSignUpButton.setOnClickListener(this);
+        mSignInButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mSignUpButton){
+            userSignUp();
+        }
+        else if(v == mSignInButton) {
+            startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+            finish();
+        }
+    }
+
+    private void userSignUp(){
+
+        String userEmail, userPassword, userConfirmPassword;
+        userEmail           = mEmail.getText().toString().trim();
+        userPassword        = mPassword.getText().toString().trim();
+        userConfirmPassword = mConfirmPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(userEmail)){
+            Toast.makeText(SignUpActivity.this,"Enter Email",Toast.LENGTH_SHORT).show();
+            return;
+        } else if(TextUtils.isEmpty(userPassword)){
+            Toast.makeText(SignUpActivity.this,"Enter Password",Toast.LENGTH_SHORT).show();
+            return;
+        } else if(TextUtils.isEmpty(userConfirmPassword)){
+            Toast.makeText(SignUpActivity.this,"Enter Confirm Password",Toast.LENGTH_SHORT).show();
+            return;
+        }else if(userPassword.length() < 6 ){
+            Toast.makeText(SignUpActivity.this,"Password must be greater than 6 letters!",Toast.LENGTH_SHORT).show();
+            return;
+        } else if(!userPassword.equals(userConfirmPassword)) {
+            Toast.makeText(SignUpActivity.this,"Passwords do not match!",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        mDialog.setMessage("Creating User! Please wait...");
+        mDialog.setCanceledOnTouchOutside(false);
+        mAuth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    mDialog.dismiss();
+                    startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Error on creating user!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
