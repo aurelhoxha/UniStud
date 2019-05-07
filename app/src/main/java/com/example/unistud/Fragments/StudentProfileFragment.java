b71456dc9bb3610d10922aba1b92933f21631ec3
gjuzialbjon <albjon.gjuzi@ug.bilkent.edu.tr>
2019-05-07 03:56:38 +0300

    profile

--- app/src/main/java/com/example/unistud/Fragments/StudentProfileFragment.java
+++ app/src/main/java/com/example/unistud/Fragments/StudentProfileFragment.java
@@ -12,9 +12,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.unistud.Activities.PlayVideo;
+import com.example.unistud.Activities.ProfileEvents;
+import com.example.unistud.Activities.ProfileInternships;
+import com.example.unistud.Activities.ProfileItems;
+import com.example.unistud.Activities.ProfileTutorials;
 import com.example.unistud.Activities.TutorialLiveStream;
 import com.example.unistud.Helpers.Tutorial;
 import com.example.unistud.Helpers.TutorialViewHolder;
@@ -43,6 +48,15 @@ public class StudentProfileFragment extends Fragment {
     private FirebaseUser mFirebaseUser;
     private String userId;
 
+    private TextView tutorialsnr;
+    private TextView tutorialstxt;
+    private TextView eventsnr;
+    private TextView eventstxt;
+    private TextView internshipsnr;
+    private TextView internshipstxt;
+    private TextView itemsnr;
+    private TextView itemstxt;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -51,18 +65,129 @@ public class StudentProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         View myFragmentView = inflater.inflate(R.layout.fragment_student_profile, container, false);
 
-        mFirebaseAuth = FirebaseAuth.getInstance();
-        mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        userId = mFirebaseUser.getUid();
+        //mFirebaseAuth = FirebaseAuth.getInstance();
+        //mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        //userId = mFirebaseUser.getUid();
+
+        tutorialsnr = (TextView)myFragmentView.findViewById(R.id.tutorial_nr);
+        tutorialsnr.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),"Tuts1", Toast.LENGTH_LONG).show();
+
+                Intent intent = new Intent(getApplicationContext(), ProfileTutorials.class);
+                //intent.putExtra(TUTORIAL_ID, tutorialID);
+                startActivityForResult(intent,1);
+
+            }
+        });
+
+        tutorialstxt = (TextView)myFragmentView.findViewById(R.id.tutorial_txt);
+        tutorialstxt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),"Tuts2", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), ProfileTutorials.class);
+                //intent.putExtra(TUTORIAL_ID, tutorialID);
+                startActivityForResult(intent,1);
+
+            }
+        });
+
+        eventsnr = (TextView)myFragmentView.findViewById(R.id.event_nr);
+        eventsnr.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),"Eve1", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), ProfileEvents.class);
+                //intent.putExtra(TUTORIAL_ID, tutorialID);
+                startActivityForResult(intent,1);
 
 
+            }
+        });
+
+        eventstxt = (TextView)myFragmentView.findViewById(R.id.event_txt);
+        eventstxt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),"Eve2", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), ProfileEvents.class);
+                //intent.putExtra(TUTORIAL_ID, tutorialID);
+                startActivityForResult(intent,1);
+
+            }
+        });
+
+        internshipsnr = (TextView)myFragmentView.findViewById(R.id.internship_nr);
+        internshipsnr.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),"Int1", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), ProfileInternships.class);
+                //intent.putExtra(TUTORIAL_ID, tutorialID);
+                startActivityForResult(intent,1);
+
+
+            }
+        });
+
+        internshipstxt = (TextView)myFragmentView.findViewById(R.id.internship_txt);
+        internshipstxt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),"Int2", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), ProfileInternships.class);
+                //intent.putExtra(TUTORIAL_ID, tutorialID);
+                startActivityForResult(intent,1);
+
+            }
+        });
+
+        itemsnr = (TextView)myFragmentView.findViewById(R.id.item_nr);
+        itemsnr.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),"Ite1", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), ProfileItems.class);
+                //intent.putExtra(TUTORIAL_ID, tutorialID);
+                startActivityForResult(intent,1);
+
+            }
+        });
+
+        itemstxt = (TextView)myFragmentView.findViewById(R.id.item_txt);
+        itemstxt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                Toast.makeText(getApplicationContext(),"Ite2", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), ProfileItems.class);
+                //intent.putExtra(TUTORIAL_ID, tutorialID);
+                startActivityForResult(intent,1);
+
+            }
+        });
 
         //Return The View
         return myFragmentView;
     }
-
-
-
-
-
 }
+
+
+
